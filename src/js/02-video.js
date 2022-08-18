35e9af26609d7780a79b94234c5f86f3bfb18a10
vitaliy-kro <krochakv6@gmail.com>
2022-08-18 16:48:12 +0300

    completed 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,11 +11,9 @@ player
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
